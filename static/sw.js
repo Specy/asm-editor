2c4f56c36b7610ed1e791384f3e16ee435bdcdce
Specy <specy.dev@gmail.com>
2023-04-14 17:01:37 +0200

    change sw

--- static/sw.js
+++ static/sw.js
@@ -1,19 +1,37 @@
 /* eslint-disable */
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
+const VERSION = 'v1';
 
-  workbox.routing.registerRoute(
-    new RegExp('/*'),
-    new workbox.strategies.NetworkFirst()
+
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.NetworkFirst({
+    cacheName: `static-${VERSION}`,
+  })
+);
+
+
+self.addEventListener('install', (evt) => {
+  console.log('[ServiceWorker] Install');
+  self.skipWaiting();
+});
+
+
+self.addEventListener('activate', (evt) => {
+  console.log('[ServiceWorker] Activate');
+  //Remove previous cached data from disk.
+  evt.waitUntil(
+    caches.keys().then(async (keyList) => {
+      const promises = await Promise.all(keyList.map((key) => {
+        if (key !== `static-${VERSION}`) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key)
+        }
+        return new Promise(resolve => resolve())
+      }));
+      return promises
+    })
   );
-
-
-  self.addEventListener('install', (evt) => {
-    console.log('[ServiceWorker] Install');
-    self.skipWaiting();
-  });
-
-  self.addEventListener('activate', (evt) => {
-    console.log('[ServiceWorker] Activate');
-    self.clients.claim();
-  });
+  self.clients.claim();
+});
