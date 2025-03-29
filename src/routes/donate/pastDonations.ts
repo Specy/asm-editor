f61fde15b931694055d2eff69e325b7d6b0deb9e
Specy <specy.dev@gmail.com>
2025-03-29 19:17:08 +0100

    typo

--- src/routes/donate/pastDonations.ts
+++ src/routes/donate/pastDonations.ts
@@ -5,7 +5,7 @@ export const PAST_DONATIONS = [
     {
         from: 'Dunat',
         amount: 0.05,
-        message: 'Grazie per il tuo importanate contributo.',
+        message: 'Grazie per il tuo importante contributo.',
         date: new Date('03/28/2025'),
     }
 ]
