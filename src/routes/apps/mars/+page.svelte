c1a7e5550f2c48b027b06d9617db69a3760d4632
Specy <specy.dev@gmail.com>
2025-05-28 14:49:08 +0200

    Add mars with cheerpj

---
+++ src/routes/apps/mars/+page.svelte
@@ -0,0 +1,70 @@
+
+
+<script lang="ts">
+
+    import { onMount } from 'svelte'
+    import DefaultNavbar from '$cmp/shared/layout/DefaultNavbar.svelte'
+    import Page from '$cmp/shared/layout/Page.svelte'
+
+    let CHEERP = $state<null | {
+        cheerpjRunJar: (jarUrl: string) => void,
+        cheerpjCreateDisplay: (
+            width?: number,
+            height?: number,
+            container?: HTMLElement
+        ) => void
+    }>(null)
+    onMount(() => {
+
+        async function onload() {
+            if (!('cheerpjRunJar' in window)) {
+                await (window as any).cheerpjInit({version: 11})
+            }
+            CHEERP = {
+                cheerpjRunJar: (window as any).cheerpjRunJar,
+                cheerpjCreateDisplay: (window as any).cheerpjCreateDisplay
+            }
+        }
+
+        if ('cheerpjInit' in window) {
+            onload()
+        } else {
+            const script = document.createElement('script')
+            script.src = 'https://cjrtnc.leaningtech.com/4.1/loader.js'
+            script.async = true
+            document.head.appendChild(script)
+            script.onload = onload
+        }
+    })
+
+    let wrapper = $state<HTMLElement | null>(null)
+
+    $inspect(CHEERP)
+
+    $effect(() => {
+        if (CHEERP && wrapper) {
+            CHEERP.cheerpjCreateDisplay(
+                -1,
+                -1,
+                wrapper
+            )
+            CHEERP.cheerpjRunJar('/app/native-apps/mars.jar')
+        }
+    })
+</script>
+
+<DefaultNavbar />
+
+<Page hasNavbar>
+	<div bind:this={wrapper} class="wrapper">
+
+	</div>
+</Page>
+
+<style>
+    .wrapper {
+        display: flex;
+        flex: 1;
+    }
+</style>
+
